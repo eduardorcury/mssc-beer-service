16230f0b91b50412176351bd31098c4722bdac7a
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-09-14 13:53:36 -0400

    Adicioando controlador

---
+++ src/main/java/com/erc/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,32 @@
+package com.erc.msscbeerservice.web.controller;
+
+import com.erc.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto) {
+        //TODO impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        //TODO impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+
+}
