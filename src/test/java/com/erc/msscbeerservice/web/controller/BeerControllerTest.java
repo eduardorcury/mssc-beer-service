135b20bde6b5d651f07630e96b3dd93a221cfe19
Eduardo Ribeiro <eduardoribeirocury@gmail.com>
2020-11-23 19:14:21 -0400

    Documentação para POST /saveNewBeer

--- src/test/java/com/erc/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/erc/msscbeerservice/web/controller/BeerControllerTest.java
@@ -24,8 +24,7 @@ import static org.mockito.BDDMockito.*;
 // import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -82,8 +81,19 @@ class BeerControllerTest {
         mockMvc.perform(post("/api/v1/beer/")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
-                .andExpect(status().isCreated());
-
+                .andExpect(status().isCreated())
+                .andDo(document("v1/beer",
+                        requestFields(
+                                fieldWithPath("id").ignored(),
+                                fieldWithPath("version").ignored(),
+                                fieldWithPath("createdDate").ignored(),
+                                fieldWithPath("lastModifiedDate").ignored(),
+                                fieldWithPath("beerName").description("Name of the Beer"),
+                                fieldWithPath("beerStyle").description("Style of the Beer"),
+                                fieldWithPath("upc").description("UPC of the Beer").attributes(),
+                                fieldWithPath("price").description("Price of the Beer"),
+                                fieldWithPath("quantityOnHand").ignored()
+                        )));
     }
 
     @Test
